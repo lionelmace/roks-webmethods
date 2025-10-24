e327fd446724f446daf35516800f91c05c321ae6
Lionel Mace <lionel.mace@gmail.com>
2025-10-24 17:33:33 +0200

    removed provider for registry

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -27,22 +27,27 @@ provider "helm" {
   }]
 }
 
-# Data source to retrieve token details
-data "ibm_iam_auth_token" "token_data" {
-}
 
-# Data source to account settings
-data "ibm_iam_account_settings" "iam_account_settings" {
-}
+########################################################################################################################
+# Container Registry
+########################################################################################################################
 
-provider "restapi" {
-  uri                   = "https:"
-  write_returns_object  = false
-  create_returns_object = false
-  debug                 = false # set to true to show detailed logs, but use carefully as it might print sensitive values.
-  headers = {
-    Account       = data.ibm_iam_account_settings.iam_account_settings.account_id
-    Authorization = data.ibm_iam_auth_token.token_data.iam_access_token
-    Content-Type  = "application/json"
-  }
-}
+# # Data source to retrieve token details
+# data "ibm_iam_auth_token" "token_data" {
+# }
+
+# # Data source to account settings
+# data "ibm_iam_account_settings" "iam_account_settings" {
+# }
+
+# provider "restapi" {
+#   uri                   = "https:"
+#   write_returns_object  = false
+#   create_returns_object = false
+#   debug                 = false # set to true to show detailed logs, but use carefully as it might print sensitive values.
+#   headers = {
+#     Account       = data.ibm_iam_account_settings.iam_account_settings.account_id
+#     Authorization = data.ibm_iam_auth_token.token_data.iam_access_token
+#     Content-Type  = "application/json"
+#   }
+# }
